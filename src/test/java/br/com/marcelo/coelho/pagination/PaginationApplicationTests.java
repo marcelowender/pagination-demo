ad5f4e7876e3fc669935461734885a19f2f93c24
marcelowender <marcelowender@hotmail.com>
2019-12-09 23:04:39 -0300

    First commit

---
+++ src/test/java/br/com/marcelo/coelho/pagination/PaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelo.coelho.pagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
